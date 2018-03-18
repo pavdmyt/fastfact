c0f58f6c54ce8863197e12c41384cde37dc08759
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-03-18 21:18:11 +0200

    simplify IterFact

--- iterative.go
+++ iterative.go
@@ -12,16 +12,5 @@ func IterFact(n uint64) *big.Int {
 		retval, _ := miniFact(n)
 		return retval
 	}
-
-	// n > 2
-	one := big.NewInt(1)
-	bigN := big.NewInt(int64(n))
-	res := big.NewInt(1)
-	i := big.NewInt(2)
-
-	for i.Cmp(bigN) != 1 { // i <= bigN
-		res.Mul(res, i) // res *= i
-		i.Add(i, one)   // i++
-	}
-	return res
+	return mulRange(1, n)
 }
